2ef1f4b2af9f0eecbe761549687687749056dd25
avas <44698603+skh6075@users.noreply.github.com>
2021-11-10 14:47:01 +0900

    Add files via upload

--- baekjoon_1110.kts
+++ baekjoon_1110.kts
@@ -1,29 +1,43 @@
-import java.io.BufferedReader
-import java.io.InputStreamReader
-import java.util.*
+import java.io.*
 
 fun main() {
     var input = BufferedReader(InputStreamReader(System.`in`))
-    var num: Int = input.readLine().toInt()
-    var str: String = num.toString()
-    var num1: Int = str.substring(0, 1).toInt()
-    var num2: Int = str.substring(str.length -1).toInt()
-
-    var checkNumber = num1 + num2
+    var number: Int = input.readLine().toInt()
+    var copy: Int = number
     var count: Int = 0
-    while (true) {
-        var now = num1 + num2
-        var toStr: String = now.toString()
-        var lastNumber: Int = toStr.substring(toStr.length - 1).toInt()
 
-        num1 = num2
-        num2 = lastNumber
+    do{
+        number = number % 10 * 10 + (number / 10 + number % 10) % 10
+        count ++
+    }while (copy != number)
 
-        now = num1 + num2
-        count++
-        if(checkNumber == now)
-            break
-    }
     println(count)
 }
 main()
+//
+//
+//   아래 소스도 작동함
+//   시간 초과로 다시 만듬
+//
+//
+//    var str: String = num.toString()
+//    var num1: Int = str.substring(0, 1).toInt()
+//    var num2: Int = str.substring(str.length -1).toInt()
+//
+//    var checkNumber = num1 + num2
+//    var count: Int = 0
+//    while (true) {
+//        var now = num1 + num2
+//        var toStr: String = now.toString()
+//        var lastNumber: Int = toStr.substring(toStr.length - 1).toInt()
+//
+//        num1 = num2
+//        num2 = lastNumber
+//
+//        now = num1 + num2
+//        count++
+//        if(checkNumber == now)
+//            break
+//    }
+//    println(count)
+//}
